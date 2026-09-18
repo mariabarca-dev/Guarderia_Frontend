3b02b0e49a0e4a2754815f33fb5e90288e8d1a71
mariabarca-dev <maria.barcal@alu.inspt.utn.edu.ar>
2026-09-17 23:18:33 -0300

    Primer commit proyecto web Java

---
+++ src/main/java/com/GuarderiaCentral/Guarderia_Frontend/GuarderiaFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.GuarderiaCentral.Guarderia_Frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuarderiaFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuarderiaFrontendApplication.class, args);
+	}
+
+}
