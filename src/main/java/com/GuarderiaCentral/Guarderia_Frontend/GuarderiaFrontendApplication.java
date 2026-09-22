f17667be2f10d92b424f90bce1149185aeb5ef53
dforclaz <daniela.forclaz@alu.inspt.utn.edu.ar>
2026-09-22 00:07:09 -0300

    Primeros cambios Front

--- src/main/java/com/GuarderiaCentral/Guarderia_Frontend/GuarderiaFrontendApplication.java
+++ src/main/java/com/GuarderiaCentral/Guarderia_Frontend/GuarderiaFrontendApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//hola
 public class GuarderiaFrontendApplication {
 
 	public static void main(String[] args) {
